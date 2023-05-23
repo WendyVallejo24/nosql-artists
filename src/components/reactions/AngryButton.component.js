585640058f0b47e8125c1d79ad9bb21db758e255
WendyVallejo24 <wbvalpat24@gmail.com>
2023-05-23 03:50:19 -0600

    update reactions

--- src/components/reactions/AngryButton.component.js
+++ src/components/reactions/AngryButton.component.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
 import angry from '../img/angry.png';
+import KafkaService from "./../../services/kafka.service";
+
+function saveLike(e, status) {
+
+    let data = {
+        id: 0,
+        status: status
+    };
+
+    console.log(JSON.stringify(data));
+
+    KafkaService.reaction("angry");
+    e.preventDefault();
+}
 
 function AngryButton() {
     const [angries, setLikes] = useState(0);
@@ -8,9 +22,12 @@ function AngryButton() {
         <div className="like-button-container">
             <button id="like"
                 className={`like-button ${angried ? 'liked' : ''}`}
-                onClick={() => {
+                onClick={(e) => {
                     setLikes(angries + 1);
                     setLiked(true);
+
+                    e.preventDefault();
+                    saveLike(e, 1)
                 }}
             >
                 <img src={angry} className='img' width={15} height={15} alt="" /> <br/>
